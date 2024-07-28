8c3f7161791098eeaed04468fa25aa183fef8090
rosemolt <rosemolthomas02@gmail.com>
2024-07-28 07:59:04 -0700

    signin completed

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -76,7 +76,7 @@ const SignUp = () => {
                             <button onClick={readValue} className="btn btn-success">Register</button>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <a href="" className="btn btn-primary">Back to Login</a>
+                            <a href="/" className="btn btn-primary">Back to Login</a>
                         </div>
                     </div>
                 </div>
